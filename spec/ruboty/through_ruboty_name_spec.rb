047a868fc37738c0979dcd6bd7edc80121b48ff7
manga_osyo <manga.osyo@gmail.com>
2016-07-02 15:05:48 +0900

    Fix spec.

--- spec/ruboty/through_ruboty_name_spec.rb
+++ spec/ruboty/through_ruboty_name_spec.rb
@@ -4,8 +4,4 @@ describe Ruboty::ThroughRubotyName do
   it 'has a version number' do
     expect(Ruboty::ThroughRubotyName::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
